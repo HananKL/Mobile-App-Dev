c4e7c08db0dad71d7bd375c27d170c6808c8c796
HananKL <159758443+HananKL@users.noreply.github.com>
2024-09-30 22:12:12 -0700

    Add files via upload

---
+++ lab3.dart
@@ -0,0 +1,142 @@
+/*void main() {
+  List<int> myIntList = [];
+  myIntList.add(1);
+  myIntList.add(2);
+  myIntList.add(3);
+  print('List of Integers: $myIntList');
+
+  List<String> myStringList = [];
+  myStringList.add('Hello');
+  myStringList.add('World');
+  print('List of Strings: $myStringList');
+}*/
+
+/*void main() {
+  List<String> fruits = ['Apple', 'Banana', 'Cherry', 'Mango'];
+
+  print('First fruit: ${fruits[0]}');
+  print('Second fruit: ${fruits[1]}');
+  print('Third fruit: ${fruits[2]}');
+  print('Fourth fruit: ${fruits[3]}');
+
+  print('All fruits:');
+  for (int i = 0; i < fruits.length; i++) {
+    print(fruits[i]);
+  }
+}*/
+
+/*void main() {
+  List<String> colors = ['Red', 'Green', 'Blue'];
+  print('list of colors: $colors');
+  colors.add('Yellow');
+  print('Updated list of colors: $colors');
+}*/
+
+/*void main() {
+  List<String> fruits = ['Apple', 'Banana', 'Cherry', 'Date'];
+  print('list of fruits: $fruits');
+  fruits.remove('Banana');
+  print('Updated list of fruits: $fruits');
+}*/
+
+/*void main() {
+  List<String> animals = ['Dog', 'Cat', 'Elephant', 'Tiger'];
+
+  print('Number of animals: ${animals.length}');
+}*/
+
+/*void main() {
+  Map<String, int> scores = {};
+
+  scores['Alice'] = 90;
+  scores['Bob'] = 85;
+
+  print('Scores: $scores');
+}*/
+
+/*void main() {
+  Map<String, int> scores = {
+    'Alice': 90,
+    'Bob': 85,
+    'Charlie': 92,
+  };
+
+  print('Scores: $scores');
+}*/
+
+/*void main() {
+  Map<String, int> scores = {
+    'Alice': 90,
+    'Bob': 85,
+    'Charlie': 92,
+  };
+
+  int aliceScore = scores['Alice']!;
+  int bobScore = scores['Bob']!;
+
+  print('Alice\'s Score: $aliceScore');
+  print('Bob\'s Score: $bobScore');
+}*/
+
+/*void main() {
+  Map<String, int> scores = {
+    'Alice': 90,
+    'Bob': 85,
+  };
+  print('Scores: $scores');
+  scores['Charlie'] = 92;
+  print('Updated Scores: $scores');
+}*/
+
+/*void main() {
+  Map<String, int> scores = {
+    'Alice': 90,
+    'Bob': 85,
+    'Charlie': 92,
+  };
+
+  print('Number of entries in the map: ${scores.length}');
+}*/
+
+/*void main() {
+  int a = 15;
+  int b = 7;
+
+  int sum = a + b;
+
+  print('The sum of $a and $b is: $sum');
+}*/
+
+/*void main() {
+  double b = 3.2;
+  double a = 10.5;
+  double product = a * b;
+
+  print('The product of $a and $b is: $product');
+}*/
+
+/*void main() {
+  int number = -8;
+  int absoluteValue = number.abs();
+  
+  print('The absolute value of $number is $absoluteValue');
+}*/
+
+/*void main() {
+  double number = 7.3;
+
+  int ceilingValue = number.ceil();
+  int floorValue = number.floor();
+
+  print('The ceiling value of $number is $ceilingValue');
+  print('The floor value of $number is $floorValue');
+}*/
+
+/*void main() {
+  int a = 30;
+  int b = 20;
+
+  int greaterValue = (a > b) ? a : b;
+
+  print('The greater value is $greaterValue');
+}*/
